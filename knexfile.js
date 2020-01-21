4f009f73494f7af14307942a1f14f90aabe421db
Michelle Scott <scottmm374@gmail.com>
2020-01-21 14:09:24 -0500

    initial setup, adding dependancies

---
+++ knexfile.js
@@ -0,0 +1,22 @@
+module.exports = {
+  development: {
+    client: "sqlite3",
+    useNullAsDefault: true,
+    connection: {
+      filename: "./data/users.db3"
+    },
+    migrations: {
+      directory: "./migrations"
+    },
+    seeds: {
+      directory: "./seeds"
+    },
+
+    pool: {
+      afterCreate: (conn, done) => {
+        // runs after a connection is made to the sqlite engine
+        conn.run("PRAGMA foreign_keys = ON", done); // turn on FK enforcement
+      }
+    }
+  }
+};
